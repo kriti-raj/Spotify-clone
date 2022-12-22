3328f5f650b820e6c8a48c850572ed46f7069c61
Kriti Raj <6517kritiraj@gmail.com>
2022-12-22 05:36:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log("Welcome to Spotify");
+let songs = [
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+  {
+    songName: "Salam-e-Isaq",
+    filePath: "Data/songs/1.mp3",
+    coverPath: "Data/covers/1.jpg",
+  },
+];
+
+let audioElement = new Audio("1.mp3");
+// audioElement.play;
+
+// Listen to Events
+document
